98558e88fe9edf6fb63359f8bba469461e3b060f
latruonghai <53855632+latruonghai@users.noreply.github.com>
2020-07-17 12:29:25 +0700

    Update FaceDetect.py

--- FaceDetect/FaceDetect.py
+++ FaceDetect/FaceDetect.py
@@ -42,59 +42,58 @@ def rotation(img, angle):
 
 
 # In[14]:
+ def detectFace():
+    # Doc hinh
+    #hai = ['./MTPs/MTP'+str(i)+'.jpg' for i in range(1,188) if i not in [89,152]]
+    files = input("Nhap ten Folder : ")
+    createf = files.replace(".com","")
+    name_file = input("Nhập tên file đi nào: ")
+    name_files = input("Nhập tên file cần test ")
+    sampleNum = 0
+    path = '/home/lahai/Downloads/MyTam/'
+    path_dir = "/home/lahai/Desktop/OpenCV"
+    pathFolder = path + name_files
+    for img1 in os.listdir(pathFolder):
+        file_name = pathFolder + '/'+img1
+        img = cv2.imread(file_name)
+        #print(img.shape)
+        #img = cv2.equalizeHist(img)
+        #img = cv2.resize(img,(400,400))
+        img = cv2.resize(img,(400,int(img.shape[0]*400/img.shape[1])))
+        # Covert ảnh từ BGR sang YUV
+        img_yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
+        # Trích xuất phần đặc trưng trong ảnh (các, phần tử quan tronjg)
+        img_yuv[:,:,0] = cv2.equalizeHist(img_yuv[:,:,0])
 
-
-# Doc hinh
-#hai = ['./MTPs/MTP'+str(i)+'.jpg' for i in range(1,188) if i not in [89,152]]
-files = input("Nhap ten Folder : ")
-createf = files.replace(".com","")
-name_file = input("Nhập tên file đi nào: ")
-name_files = input("Nhập tên file cần test ")
-sampleNum = 0
-path = '/home/lahai/Downloads/MyTam/'
-path_dir = "/home/lahai/Desktop/OpenCV"
-pathFolder = path + name_files
-for img1 in os.listdir(pathFolder):
-    file_name = pathFolder + '/'+img1
-    img = cv2.imread(file_name)
-    #print(img.shape)
-    #img = cv2.equalizeHist(img)
-    #img = cv2.resize(img,(400,400))
-    img = cv2.resize(img,(400,int(img.shape[0]*400/img.shape[1])))
-    # Covert ảnh từ BGR sang YUV
-    img_yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
-    # Trích xuất phần đặc trưng trong ảnh (các, phần tử quan tronjg)
-    img_yuv[:,:,0] = cv2.equalizeHist(img_yuv[:,:,0])
-
-# convert the YUV image back to RGB format
-    img_output = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
-    #grayImag = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    face = faceCasCade.detectMultiScale(
-            img_output,
-            scaleFactor = 1.1,
-            minNeighbors = 5,
-            minSize = (30,30))
-    #Vẽ các đường màu xanh lá quanh khuôn mặt
-    if len(face) <=1:
-        for (x,y,w,h) in face:
-            sampleNum=sampleNum+1
-            image = img[y:y+h,x:x+w]
-            #Lưu ảnh khuôn mặt vào thư mục có tên(creatình
-            if not os.path.exists(path_dir +'/'+createf): os.mkdir(path_dir +'/'+createf)
-            # Thểm ảnh xoay
-            # Thêm ảnh ã cắt
-            try:
-                cv2.imwrite(path_dir+'/'+createf+"/"+name_file+'-'+ str(sampleNum) + ".jpg", cv2.resize(image,(216,216)))
-            except:
-                continue
-        # Hiển thị ra màn hình
-            cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0), 2)
-    #if sampleNum>60:
-     #   break
-        cv2.imshow("Face Detection ", img)
-        cv2.waitKey(1)
-cap.release()
-cv2.destroyAllWindows()
+    # convert the YUV image back to RGB format
+        img_output = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
+        #grayImag = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        face = faceCasCade.detectMultiScale(
+                img_output,
+                scaleFactor = 1.1,
+                minNeighbors = 5,
+                minSize = (30,30))
+        #Vẽ các đường màu xanh lá quanh khuôn mặt
+        if len(face) <=1:
+            for (x,y,w,h) in face:
+                sampleNum=sampleNum+1
+                image = img[y:y+h,x:x+w]
+                #Lưu ảnh khuôn mặt vào thư mục có tên(creatình
+                if not os.path.exists(path_dir +'/'+createf): os.mkdir(path_dir +'/'+createf)
+                # Thểm ảnh xoay
+                # Thêm ảnh ã cắt
+                try:
+                    cv2.imwrite(path_dir+'/'+createf+"/"+name_file+'-'+ str(sampleNum) + ".jpg", cv2.resize(image,(216,216)))
+                except:
+                    continue
+            # Hiển thị ra màn hình
+                cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0), 2)
+        #if sampleNum>60:
+         #   break
+            cv2.imshow("Face Detection ", img)
+            cv2.waitKey(1)
+    cap.release()
+    cv2.destroyAllWindows()
 
 
 # In[ ]:
